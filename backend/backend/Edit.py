7c92fb678a83c275cd54f97267ce4da8aa4a630e
wkdtldn <matteo.jsw@gmail.com>
2024-06-01 05:47:02 +0000

    :memo:update

--- backend/backend/Edit.py
+++
@@ -1,6 +0,0 @@
-def edit(dbList):
-    _dictionaryList_ = []
-    for trash in dbList:
-        container = {"name" : trash[0] ,"description" : trash[1], "disposal_method" : trash[2]}
-        _dictionaryList_.append(container)
-    return _dictionaryList_
