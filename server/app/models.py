9f236ce9bcd53439eb3a2a0a67a52386ab61344d
wkdtldn <matteo.jsw@gmail.com>
2024-06-08 16:02:59 +0900

    :memo:update

--- server/app/models.py
+++ server/app/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy import ForeignKey, Integer, Column, Text, String, Float
+from .api import db
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
