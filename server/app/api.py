6f0a78e642f58c62bae66c18d3a81f2a090bf1ff
wkdtldn <matteo.jsw@gmail.com>
2024-06-09 13:21:08 +0900

    :memo:update

--- server/app/api.py
+++ server/app/api.py
@@ -1,4 +1,4 @@
-from Model import *
+from .Model import *
 from config import *
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, request, Response
--- server/server.py
+++ server/server.py
@@ -5,7 +5,7 @@ import json
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from datetime import datetime, timedelta
 from flask_cors import CORS
-from app import api
+from app import Api
 
 app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'just_my_own_secret_key'
